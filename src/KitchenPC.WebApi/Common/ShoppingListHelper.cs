ca7b684f2f90fbd3c3b392609d8e93b1942c9f86
vitalii sokh <vitalii.sokh@digitallyinspired.com>
2020-06-05 12:13:12 +0300

    add new ShoppingList2

--- src/KitchenPC.WebApi/Common/ShoppingListHelper.cs
+++ src/KitchenPC.WebApi/Common/ShoppingListHelper.cs
@@ -13,19 +13,26 @@ namespace KitchenPC.WebApi.Common
     {
         public ShoppingListAdder CalculateServing(List<DataPlanItem> planItems, List<Recipe> recipes)
         {
+            Console.WriteLine("\n CalculateServing planItems Count ======== " + planItems.Count());
+            Console.WriteLine("\n CalculateServing recipes Count ======== " + recipes.Count());
             var newRecipe = new List<Recipe>();
             foreach (var p in planItems)
             {
+                Console.WriteLine("\n CalculateServing planItems Id ======== " + p.Id);
+                Console.WriteLine("\n CalculateServing planItems Servings ======== " + p.Servings);
+                Console.WriteLine("\n CalculateServing Dishes Count ======== " + p.Meal.Dishes.Count);
                 foreach (var d in p.Meal.Dishes)
                 {
                     var recipe = recipes.Find(p => p.Id == d.RecipeId);
-
+                    Console.WriteLine("\n CalculateServing Dishes id ======== " + d.Id);
+                    Console.WriteLine("\n CalculateServing recipe id ======== " + recipe.Id);
                     if (recipe.ServingSize < p.Servings)
                     {
+                        Console.WriteLine("\n CalculateServing recipe.ServingSize ======== " + recipe.ServingSize);
                         var newServingSize = p.Servings / recipe.ServingSize;
                         if (p.Servings % recipe.ServingSize != 0)
                             newServingSize += 1;
-
+                        Console.WriteLine("\n CalculateServing newServingSize ======== " + newServingSize);
                         foreach (var ingredient in recipe.Ingredients)
                         {
                             if (ingredient.Amount != null)
